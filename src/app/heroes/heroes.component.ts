67a7b9563724189c437693e7c0f84c9c55f10866
alex <adlitsov@gmail.com>
2018-04-25 22:05:59 +0300

    heroes.component: now storing selectedHero in class; template now consists of list of heroes and selected hero details (added click listener to heroes list elements)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,13 +8,14 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
-  heroes = HEROES;
 
   constructor() { }
 
